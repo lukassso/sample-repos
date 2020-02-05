6db6e2c6da40f4632f591a3e94f1d87b46ba2191
lukasso <lukasz.koladko@gmail.com>
2020-02-05 23:31:26 +0100

    app state

--- weather-app/src/components/App.js
+++ weather-app/src/components/App.js
@@ -1,66 +1,76 @@
-import React, { Component } from 'react';
-import Form from './Form';
-import Result from './Result';
-import './App.css';
+import React, { Component } from "react";
+import Form from "./Form";
+import Result from "./Result";
+import "./App.css";
 
-const APIKey = '5e6595efcaf37d474191d93bd3bb0ced'
+const APIKey = "5e6595efcaf37d474191d93bd3bb0ced";
 class App extends Component {
   constructor(...args) {
-    super(...args)
+    super(...args);
     this.state = {
-      value:'',
-      date: '',
-      city: '',
-      sunrise: '',
-      sunset: '',
-      temp: '',
-      pressure: '',
-      wind: '',
+      value: "",
+      date: "",
+      city: "",
+      sunrise: "",
+      sunset: "",
+      temp: "",
+      pressure: "",
+      wind: "",
       err: false
-
-  }
+    };
   }
 
   handleInputChange = e => {
     this.setState({
       value: e.target.value
-    })
-  }
+    });
+  };
   handleCitySubmit = e => {
-    e.preventDefault()
+    e.preventDefault();
     // console.log("dududud");
-    const API = 
-      `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIKey}&units=metric`;
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIKey}&units=metric`;
 
     fetch(API)
-    .then(response => {
-      if(response.ok) {
-        return response
-      }
-      throw Error("Not working")
-    })
-    .then(response => response.json())
-    .then(data => {
-      this.setState({
-        err: false
+      .then(response => {
+        if (response.ok) {
+          console.log(response);
+          return response;
+        }
+        throw Error("Not working");
       })
-    })  
-    .catch(err => {
-    console.log(err);
-    this.setState({
-      err: true
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        const time = new Date().toLocaleString();
+        this.setState(state => ({
+          err: false,
+          date: time,
+          sunrise: data.sys.sunrise,
+          sunset: data.sys.sunset,
+          temp: data.main.temp,
+          pressure: data.main.pressure,
+          wind: data.wind.speed,
+          city: data.value
+        }));
       })
-    })
-  }
+      .catch(err => {
+        console.log(err);
+        this.setState(prevState => ({
+          err: true,
+          city: prevState.value
+        }));
+      });
+  };
   render() {
     return (
       <div className="App">
         Weather app
-        <Form value={this.state.value}
+        <Form
+          value={this.state.value}
           change={this.handleInputChange}
           submit={this.handleCitySubmit}
         />
-        <Result error={this.state.err} />
+        <Result weather={this.state}  />
       </div>
     );
   }
--- weather-app/src/components/Result.js
+++ weather-app/src/components/Result.js
@@ -1,8 +1,32 @@
 import React from 'react'
 
+
+
 const Result = props => {
+  const {
+      date,
+      city,
+      sunrise,
+      sunset,
+      temp,
+      pressure,
+      wind,
+      err,} = props.weather;
+
+  let content = null;
+
+  if(!err && city) {
+    content = (
+      <div>{`Wyszukiwanie dla miasta ${city}`}</div>
+    )
+  }
+
   return (
-    <div>{props.error}</div>
+    <>
+    <div className="result">
+      {err ? `Nie ma w bazie ${city}` : content}
+    </div>
+    </>
   );
 }
 
