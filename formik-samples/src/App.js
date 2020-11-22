f6678dbd6d48770ed2e8a9e6f2722dba01f6e465
lukasso <lukasz.koladko@gmail.com>
2020-11-22 18:16:55 +0100

    clean validation for name and email inputs

--- formik-samples/src/App.js
+++ formik-samples/src/App.js
@@ -4,12 +4,25 @@ import { useFormik } from "formik";
 
 import { Button, Card, Form, Container, Row, Col } from "react-bootstrap";
 
+const EMAIL_REG = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
 const validate = (values) => {
   const errors = {};
 
-  if (!values.firstName.lenght) {
+  if (!values.firstName.length) {
     errors.firstName = "name is required!";
+  } else if (values.firstName.length < 3) {
+    errors.firstName = "name is too short";
+  } else if (values.firstName.length > 12) {
+    errors.firstName = "name is too long. Max 11 charts";
   }
+
+  if (!values.email.length) {
+    errors.email = "email is required";
+  }else if (!values.email.match(EMAIL_REG)){
+    errors.email = "email is not correct"
+  }
+ 
   return errors;
 };
 
@@ -30,11 +43,13 @@ function App() {
     <div>
       <p>{formik.errors.firstName}</p>
     </div>
-  ) : (
+  ) : null;
+
+  const emailError = formik.errors.email ? (
     <div>
-      <p>{formik.errors.firstName}</p>
+      <p>{formik.errors.email}</p>
     </div>
-  );
+  ) : null;
 
   return (
     <Card className="text-center">
@@ -52,11 +67,26 @@ function App() {
               onChange={formik.handleChange}
               value={formik.values.firstName}
               placeholder="name"
+              // className={formik.errors.firstName ? 'red' : null}
             />
           </label>
-          <button type="submit">Send</button>
-        </form>
+        
         {firstNameError}
+
+        <label>
+          Email:
+          <input
+            onChange={formik.handleChange}
+            name="email"
+            value={formik.values.email}
+            placeholder="email"
+            type="email"
+            required
+          />
+        </label>
+        <button type="submit">Send</button>
+        {emailError}
+        </form>
 
         {/* <Form onSubmit={formik.handleSubmit}>
           <Form.Group controlId="firstName">
