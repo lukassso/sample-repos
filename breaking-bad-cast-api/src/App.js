0733a10adde35fd6e3a5c026ca1047ef345494bc
lukasso <lukasz.koladko@gmail.com>
2021-01-25 21:49:47 +0100

    api data connection

--- breaking-bad-cast-api/src/App.js
+++ breaking-bad-cast-api/src/App.js
@@ -8,15 +8,29 @@ import './App.css'
 const App = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
-    
-  }, [])
+    const fetchItems = async () => {
+      
+      setIsLoading(true)
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      )
+
+      // console.log(result.data)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [query])
 
   return (
     <div className='container'>
       <Header />
-      <Search />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   )
